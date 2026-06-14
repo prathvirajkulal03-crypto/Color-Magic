8ed7a6a082ed4dea325ba1286a72067de8da178e
prathvirajkulal03-crypto <prathvirajkulal03@gmail.com>
2026-06-14 12:53:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("changeBtn");
+const colorCode = document.getElementById("colorCode");
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+button.addEventListener("click", () => {
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+    colorCode.textContent = newColor;
+});
